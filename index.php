16e52215ca40a05bc5605ee2a9d3fe97c0166197
Nikolai Ivanov <mraiur@gmail.com>
2015-06-11 20:52:03 +0300

    Added html header footer functions.

--- index.php
+++ index.php
@@ -1,19 +1,45 @@
 <?php
 require_once "lib/Validate.php";
-
-$_POST['username'] = 'mraiur';
-
+require_once "functions/html.php";
 
 $contoller = Validate::get('controller','string', 'index');
+$action = Validate::get('action', 'string', 'index');
 
-echo "product_id: ";
-echo Validate::get('product_id','number', 0);
-echo "<br />";
+if($contoller == 'index'){
+    html_header();
+    echo "Home";
 
-echo "controller: ";
-echo $contoller;
-echo "<br />";
+    if($action == "index"){
+        echo "-> index";
+    }
 
-echo "username: ";
-echo Validate::post('username', 'string');
-echo "<br />";
+    html_footer();
+
+} else if($contoller == 'user') {
+
+    html_header();
+
+    echo "User";
+    if($action == 'login'){
+        ?>
+        <form>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            </div>
+
+            <button type="submit" class="btn btn-default">Submit</button>
+        </form>
+        <?php
+    } else {
+        echo "-> index";
+    }
+
+    html_footer();
+} else {
+    echo "No page for ".$contoller;
+}
